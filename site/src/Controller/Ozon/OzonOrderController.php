a066d8e3ca71225a0c918bb899525b6e02ece819
pavelsur07 <pavelsur07@gmail.com>
2025-09-07 11:41:55 +0300

    Add manual Ozon orders sync

--- site/src/Controller/Ozon/OzonOrderController.php
+++ site/src/Controller/Ozon/OzonOrderController.php
@@ -6,11 +6,15 @@ use App\Entity\Ozon\OzonOrder;
 use App\Entity\Ozon\OzonOrderItem;
 use App\Repository\Ozon\OzonOrderRepository;
 use App\Repository\Ozon\OzonOrderStatusHistoryRepository;
+use App\Service\Ozon\OzonOrderSyncService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use DateInterval;
+use DateTimeImmutable;
+use DateTimeZone;
 
 class OzonOrderController extends AbstractController
 {
@@ -45,6 +49,18 @@ class OzonOrderController extends AbstractController
         return $this->render('ozon/orders/index.html.twig', [
             'orders' => $orders,
         ]);
+    }
+
+    #[Route('/ozon/orders/sync', name: 'ozon_orders_sync')]
+    public function sync(OzonOrderSyncService $syncService): Response
+    {
+        $company = $this->getUser()->getCompanies()[0];
+        $to = new DateTimeImmutable('now', new DateTimeZone('UTC'));
+        $since = $to->sub(new DateInterval('P3D'));
+        $syncService->syncFbs($company, $since, $to);
+        $syncService->syncFbo($company, $since, $to);
+        $this->addFlash('success', 'Ozon заказы обновлены!');
+        return $this->redirectToRoute('ozon_orders');
     }
 
     #[Route('/ozon/orders/{id}', name: 'ozon_order_show')]
