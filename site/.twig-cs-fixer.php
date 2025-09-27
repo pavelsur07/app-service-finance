efa4425091b4f816b9a25b49e73b5d629a9bbe66
Pavel Novikov <pavelsur07@gmail.com>
2025-09-27 15:57:28 +0300

    setting cs-fixer #1

--- site/.twig-cs-fixer.php
+++ site/.twig-cs-fixer.php
@@ -1,9 +1,35 @@
 <?php
+declare(strict_types=1);
 
 use FriendsOfTwig\Twigcs\Config\Config;
 use FriendsOfTwig\Twigcs\Ruleset\Official;
+use Symfony\Component\Finder\Finder;
 
-return (new Config())
-    ->setSeverity('warning') // 'warning' или 'error'
-    ->setRuleset(new Official())
-    ->setPaths([__DIR__ . '/templates']);
+// Мажорная версия Twig (например, 3)
+$version   = \class_exists(\Twig\Environment::class) ? (string) \Twig\Environment::VERSION : '3.0.0';
+$twigMajor = (int) explode('.', $version)[0];
+
+$config = new Config();
+
+// ВАЖНО: строка-класс ruleset'а
+$config->setRuleset(Official::class);
+
+// В новых версиях — setTwigMajorVersion(); в более старых — setTwigVersion()
+if (method_exists($config, 'setTwigMajorVersion')) {
+    $config->setTwigMajorVersion($twigMajor);
+} elseif (method_exists($config, 'setTwigVersion')) {
+    $config->setTwigVersion($twigMajor);
+}
+
+// Используем Symfony Finder вместо FriendsOfTwig\Twigcs\Finder
+$finder = (new Finder())
+    ->files()
+    ->in(__DIR__ . '/templates')
+    ->name('*.twig')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true)
+    ->exclude(['vendor', 'var', 'node_modules']);
+
+$config->setFinder($finder);
+
+return $config;
