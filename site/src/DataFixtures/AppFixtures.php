345ba5eec931fd96839b9cca116a7fe97b52150c
Pavel Novikov <pavelsur07@gmail.com>
2025-09-27 09:01:11 +0300

    fixtures added company, counterparties, moneyAccount

--- site/src/DataFixtures/AppFixtures.php
+++ site/src/DataFixtures/AppFixtures.php
@@ -8,6 +8,7 @@ use App\Entity\MoneyAccount;
 use App\Entity\User;
 use App\Enum\CounterpartyType;
 use App\Enum\MoneyAccountType;
+use App\Service\AccountBalanceService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Ramsey\Uuid\Nonstandard\Uuid;
@@ -15,47 +16,98 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function __construct(private UserPasswordHasherInterface $passwordHasher)
-    {
+    public const REF_COMPANY_ROMASHKA = 'company.romashka';
+    public const REF_OWNER = 'user.owner';
+    public const REF_ACC_ALFA = 'money_account.alfa';
+    public const REF_ACC_CASH = 'money_account.cash';
+
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+        private AccountBalanceService $accountBalanceService,
+    ) {
     }
 
     public function load(ObjectManager $manager): void
     {
         $usersData = [
-            ['admin@app.ru', 'password', ['ROLE_ADMIN']],
+            ['admin@app.ru',   'password', ['ROLE_ADMIN']],
             ['manager@app.ru', 'password', ['ROLE_MANAGER']],
-            ['user@app.ru', 'password', ['ROLE_USER']],
+            ['user@app.ru',    'password', ['ROLE_USER']],
         ];
 
+        // ➜ Владелец owner@app.ru с ROLE_ADMIN
         $owner = new User(id: Uuid::uuid4()->toString());
         $owner->setEmail('owner@app.ru');
         $owner->setRoles(['ROLE_ADMIN']);
         $owner->setPassword($this->passwordHasher->hashPassword($owner, 'password'));
         $manager->persist($owner);
+        $this->addReference(self::REF_OWNER, $owner);
 
+        // ➜ Компания ООО "Ромашка"
         $company = new Company(id: Uuid::uuid4()->toString(), user: $owner);
-        $company->setName('Вумджой ООО');
+        $company->setName('ООО "Ромашка"');
         $manager->persist($company);
+        $this->addReference(self::REF_COMPANY_ROMASHKA, $company);
 
-        $counterparty = new Counterparty(
-            id: Uuid::uuid4()->toString(),
-            company: $company,
-            name: 'Ozon OOO',
-            type: CounterpartyType::LEGAL_ENTITY
-        );
-        $manager->persist($counterparty);
+        // --- Контрагенты для ООО "Ромашка" (6 шт., из них 2 ИП), с ИНН
+        $counterpartiesData = [
+            // Юрлица (ИНН 10 знаков)
+            ['ООО "Альфа-Снаб"',     '7708123456',   CounterpartyType::LEGAL_ENTITY],
+            ['ООО "Бета-Логистик"',  '7812345678',   CounterpartyType::LEGAL_ENTITY],
+            ['ООО "Гамма-Маркет"',   '7723456789',   CounterpartyType::LEGAL_ENTITY],
+            ['ООО "Дельта-Сервис"',  '7712345670',   CounterpartyType::LEGAL_ENTITY],
+            // Индивидуальные предприниматели (ИНН 12 знаков)
+            ['ИП Иванов И.И.',       '503212345678', CounterpartyType::INDIVIDUAL_ENTREPRENEUR],
+            ['ИП Петров П.П.',       '540812345678', CounterpartyType::INDIVIDUAL_ENTREPRENEUR],
+        ];
 
-        $moneyAccount = new MoneyAccount(
+        foreach ($counterpartiesData as [$name, $inn, $type]) {
+            $cp = new Counterparty(
+                id: Uuid::uuid4()->toString(),
+                company: $company,
+                name: $name,
+                type: $type
+            );
+            $cp->setInn($inn);
+            $manager->persist($cp);
+        }
+
+        // ➜ Денежные счета
+        // Альфа-Банк (основной)
+        $alfa = new MoneyAccount(
             id: Uuid::uuid4()->toString(),
             company: $company,
             type: MoneyAccountType::BANK,
-            name: 'Osnovnoy',
+            name: 'Альфа-Банк (основной)',
             currency: 'RUB'
         );
-        $manager->persist($moneyAccount);
+        $alfa->setAccountNumber('40702810726140001479');
+        $alfa->setOpeningBalance('1000.00');
+        $alfa->setOpeningBalanceDate(new \DateTimeImmutable('today'));
+        $manager->persist($alfa);
+        $this->addReference(self::REF_ACC_ALFA, $alfa);
+
+        // Касса (основная)
+        $cash = new MoneyAccount(
+            id: Uuid::uuid4()->toString(),
+            company: $company,
+            type: MoneyAccountType::CASH,
+            name: 'Касса (основная)',
+            currency: 'RUB'
+        );
+        $cash->setOpeningBalance('100.00');
+        $cash->setOpeningBalanceDate(new \DateTimeImmutable('today'));
+        $manager->persist($cash);
+        $this->addReference(self::REF_ACC_CASH, $cash);
 
         $manager->flush();
 
+        // ➜ Пересчёт дневных остатков на сегодняшнюю дату
+        $today = new \DateTimeImmutable('today');
+        $this->accountBalanceService->recalculateDailyRange($company, $alfa, $today, $today);
+        $this->accountBalanceService->recalculateDailyRange($company, $cash, $today, $today);
+
+        // ➜ Остальные пользователи (как было)
         foreach ($usersData as [$email, $plainPassword, $roles]) {
             $user = new User(id: Uuid::uuid4()->toString());
             $user->setEmail($email);
