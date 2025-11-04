f285867c84438f6bd7b61a83bd1b4f3839bf9814
Pavel Novikov <pavelsur07@gmail.com>
2025-11-04 20:09:20 +0300

    added scenario

---
+++ site/src/Admin/Controller/DemoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Admin\Controller;
+
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin')]
+class DemoController extends AbstractController
+{
+    #[Route('/demo', name: 'admin_demo', methods: ['GET'])]
+    public function demoOne(): Response
+    {
+        return $this->render('templates/finance/scenario/demo.html.twig');
+    }
+}
