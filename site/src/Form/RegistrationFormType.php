fb11132e5c5e2d02a3e60169f6e5636cfdaf947b
pavelsur07 <pavelsur07@gmail.com>
2025-12-31 18:58:21 +0300

    Add required company name on registration

--- site/src/Form/RegistrationFormType.php
+++ site/src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,6 +18,19 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('companyName', TextType::class, [
+                'label' => 'Имя компании',
+                'mapped' => false,
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'ООО Ромашка',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Введите имя компании',
+                    ]),
+                ],
+            ])
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'label' => 'Я принимаю Пользовательское соглашение и Политику конфиденциальности',
