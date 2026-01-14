49962ae7d8af914bcd96f7570144545feeda6da0
pavelsur07 <pavelsur07@gmail.com>
2026-01-14 19:49:45 +0300

    Add Alfa statements provider

---
+++ site/src/Cash/Service/Import/Bank/Provider/Alfa/AlfaStatementsProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Cash\Service\Import\Bank\Provider\Alfa;
+
+use App\Cash\Entity\Bank\BankConnection;
+use App\Cash\Service\Import\Bank\Provider\BankStatementsProviderInterface;
+
+class AlfaStatementsProvider implements BankStatementsProviderInterface
+{
+    public function __construct(private AlfaApiClient $apiClient)
+    {
+    }
+
+    public function getAccounts(BankConnection $connection): array
+    {
+        return $this->apiClient->getAccounts($connection);
+    }
+
+    public function getTransactions(
+        BankConnection $connection,
+        string $accountNumber,
+        \DateTimeInterface $date,
+        int $page
+    ): array {
+        return $this->apiClient->getTransactions($connection, $accountNumber, $date, $page);
+    }
+}
---
+++ site/src/Cash/Service/Import/Bank/Provider/BankStatementsProviderInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Cash\Service\Import\Bank\Provider;
+
+use App\Cash\Entity\Bank\BankConnection;
+
+interface BankStatementsProviderInterface
+{
+    public function getAccounts(BankConnection $connection): array;
+
+    public function getTransactions(
+        BankConnection $connection,
+        string $accountNumber,
+        \DateTimeInterface $date,
+        int $page
+    ): array;
+}
