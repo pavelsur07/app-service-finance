0f6612931070b5fa48425dcd7a2b2e521bfc284b
pavelsur07 <pavelsur07@gmail.com>
2026-01-21 19:18:56 +0300

    Add cash file import handler stub

---
+++ site/src/Cash/Service/Import/File/CashFileImportService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Cash\Service\Import\File;
+
+use App\Cash\Entity\Import\CashFileImportJob;
+
+final class CashFileImportService
+{
+    public function import(CashFileImportJob $job): void
+    {
+    }
+}
