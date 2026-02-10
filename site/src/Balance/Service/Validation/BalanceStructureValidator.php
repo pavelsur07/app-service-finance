185bf3b0962584d4eacce5420c428a83be3e6d62
pavelsur07 <pavelsur07@gmail.com>
2026-02-10 13:04:36 +0300

    Add balance validation service skeletons

---
+++ site/src/Balance/Service/Validation/BalanceStructureValidator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Balance\Service\Validation;
+
+final class BalanceStructureValidator
+{
+    /**
+     * @param array<mixed> $roots
+     *
+     * @return list<string>
+     */
+    public function validateTree(array $roots): array
+    {
+        return [];
+    }
+}
