53d918bf003022827b352d2e01b245b8e1f7b4ef
Pavel Novikov <pavelsur07@gmail.com>
2026-02-18 20:29:05 +0300

    added sentry

--- site/config/bundles.php
+++ site/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
