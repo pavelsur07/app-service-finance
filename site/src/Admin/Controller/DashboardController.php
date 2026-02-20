9f74ac9c1c69ee21bb94a71f655c7846b59aafa0
Pavel Novikov <pavelsur07@gmail.com>
2026-02-20 20:55:10 +0300

    test logger #2

--- site/src/Admin/Controller/DashboardController.php
+++ site/src/Admin/Controller/DashboardController.php
@@ -2,13 +2,46 @@
 
 namespace App\Admin\Controller;
 
+use App\Shared\Service\AppLogger;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 #[Route('/admin', name: 'admin_')]
 final class DashboardController extends AbstractController
 {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly AppLogger $appLogger,
+    ) {
+    }
+
+    #[Route('/_debug/log-test', name: 'debug_log_test', methods: ['GET'])]
+    public function logTest(): Response
+    {
+        // Информационный лог
+        $this->logger->info('Test INFO log for GlitchTip');
+
+        // Ошибка
+        $this->logger->error('Test ERROR ERROR log for GlitchTip My');
+
+        $startTime = microtime(true);
+
+        // ИМИТИРУЕМ долгую работу кода (останавливаем скрипт на 4 секунды)
+        sleep(4);
+
+        $duration = (int) ((microtime(true) - $startTime) * 1000);
+
+        // Теперь duration будет ~4000 мс, и условие сработает!
+        if ($duration > 3000) {
+            $this->appLogger->logSlowExecution('CashflowReportBuilder::build', $duration, 3000);
+        }
+
+        return $this->redirect('dashboard');
+    }
+
     #[Route('', name: 'dashboard', methods: ['GET'])]
     public function index(): Response
     {
--- site/src/Controller/HomeController.php
+++ site/src/Controller/HomeController.php
@@ -22,33 +22,7 @@ class HomeController extends AbstractController
         private readonly MoneyAccountDailyBalanceRepository $dailyBalanceRepository,
         private readonly CashflowReportBuilder $cashflowReportBuilder,
         private readonly MoneyAccountRepository $moneyAccountRepository,
-        private readonly LoggerInterface $logger,
-        private readonly AppLogger $appLogger,
     ) {
-    }
-
-    #[Route('/_debug/log-test', name: 'debug_log_test', methods: ['GET'])]
-    public function logTest(): Response
-    {
-        // Информационный лог
-        $this->logger->info('Test INFO log for GlitchTip');
-
-        // Ошибка
-        $this->logger->error('Test ERROR ERROR log for GlitchTip My');
-
-        $startTime = microtime(true);
-
-        // ИМИТИРУЕМ долгую работу кода (останавливаем скрипт на 4 секунды)
-        sleep(4);
-
-        $duration = (int) ((microtime(true) - $startTime) * 1000);
-
-        // Теперь duration будет ~4000 мс, и условие сработает!
-        if ($duration > 3000) {
-            $this->appLogger->logSlowExecution('CashflowReportBuilder::build', $duration, 3000);
-        }
-
-        return $this->redirect('/');
     }
 
     #[Route('/', name: 'app_home_index')]
