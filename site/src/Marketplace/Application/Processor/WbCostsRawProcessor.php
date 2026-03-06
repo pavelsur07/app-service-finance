881c226732a3ddc11d021f71bea7a9585afe0b85
pavelsur07 <pavelsur07@gmail.com>
2026-03-06 19:42:06 +0300

    Add batch-processing method to marketplace raw processors

--- site/src/Marketplace/Application/Processor/WbCostsRawProcessor.php
+++ site/src/Marketplace/Application/Processor/WbCostsRawProcessor.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Marketplace\Application\Processor;
 
+use App\Marketplace\Enum\MarketplaceType;
 use App\Marketplace\Enum\StagingRecordType;
 use App\Marketplace\Service\MarketplaceSyncService;
 
@@ -25,5 +26,14 @@ final class WbCostsRawProcessor implements MarketplaceRawProcessorInterface
     public function process(string $companyId, string $rawDocId): int
     {
         return $this->syncService->processWbCostsFromRaw($companyId, $rawDocId);
+    }
+
+    /**
+     * @param array<int, array<string, mixed>> $rawRows
+     */
+    public function processBatch(string $companyId, MarketplaceType $marketplace, array $rawRows): void
+    {
+        // TODO: Будет реализовано в PR 5 (Перевод Costs и Returns на новую архитектуру).
+        // Пока оставляем пустым, чтобы Демультиплексор мог безопасно маршрутизировать эти строки без падения.
     }
 }
