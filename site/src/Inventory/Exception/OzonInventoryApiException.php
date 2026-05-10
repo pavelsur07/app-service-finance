11be0cd45cd6e6693c56ede38af89adbebfc287d
pavelsur07 <pavelsur07@gmail.com>
2026-05-10 15:47:28 +0300

    Make OzonInventoryApiException non-final for rate-limit inheritance

--- site/src/Inventory/Exception/OzonInventoryApiException.php
+++ site/src/Inventory/Exception/OzonInventoryApiException.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace App\Inventory\Exception;
 
-final class OzonInventoryApiException extends \RuntimeException
+class OzonInventoryApiException extends \RuntimeException
 {
 }
