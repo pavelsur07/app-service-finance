df9501c7aeccda3390279012bfd2e68b9557b5b2
pavelsur07 <pavelsur07@gmail.com>
2026-05-23 16:12:53 +0300

    tests: chain dispatch expectation with Envelope callback

--- site/tests/Unit/Marketplace/MessageHandler/ProcessDayReportHandlerTest.php
+++ site/tests/Unit/Marketplace/MessageHandler/ProcessDayReportHandlerTest.php
@@ -77,10 +77,11 @@ final class ProcessDayReportHandlerTest extends TestCase
         $statusRepo = $this->createMock(MarketplaceFinancialReportSyncStatusLookupInterface::class);
         $updater = $this->createMock(WbFinancialReportSyncStatusUpdaterInterface::class);
         $bus = $this->createMock(MessageBusInterface::class);
-        $bus->expects(self::exactly(3))->method('dispatch');
-        $bus->method('dispatch')->willReturnCallback(
-            static fn (object $message, array $stamps = []): Envelope => new Envelope($message, $stamps)
-        );
+        $bus->expects(self::exactly(3))
+            ->method('dispatch')
+            ->willReturnCallback(
+                static fn (object $message, array $stamps = []): Envelope => new Envelope($message, $stamps)
+            );
         $em = $this->createMock(EntityManagerInterface::class);
 
         $handler = new ProcessDayReportHandler($repo, $bus, $em, new NullLogger(), $safe, $statusRepo, $updater);
